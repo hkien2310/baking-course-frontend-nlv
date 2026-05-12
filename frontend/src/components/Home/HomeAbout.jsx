68108b1dcd1a872cd7b7f5b26b5665cccbda874f
Hoang Kien <hkien2310@gmail.com>
2026-05-12 16:13:27 +0700

    fix(Homepage): Use API config data for HomeAbout achievements and rename 'phản hồi' to 'Liên hệ tư vấn'

--- frontend/src/components/Home/HomeAbout.jsx
+++ frontend/src/components/Home/HomeAbout.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { useTranslation } from '../../i18n/LanguageContext';
+import { useSiteConfig } from '../../context/SiteConfigContext';
+
+const FALLBACK_ACHIEVEMENTS = [
+	{ icon: 'fa-trophy', title: 'Đạt Hơn 50 Giải Thưởng Nấu Ăn', desc: 'Chứng nhận chất lượng xuất sắc, mang về các giải thưởng lớn nhỏ tại các cuộc thi làm bánh và nấu ăn trong nước cũng như quốc tế.' },
+	{ icon: 'fa-group', title: '27 Đầu Bếp - Chuyên Gia Đào Tạo', desc: 'Đội ngũ giảng viên giàu kinh nghiệm, tận tâm hướng dẫn và đồng hành cùng học viên từ những bước cơ bản nhất.' },
+	{ icon: 'fa-hourglass-half', title: 'Cam Kết Việc Làm Nhanh Chóng', desc: 'Hỗ trợ định hướng nghề nghiệp, giới thiệu học viên đến các môi trường làm việc chuyên nghiệp ngay sau khi tốt nghiệp.' },
+];
 
 const HomeAbout = () => {
-  const { t } = useTranslation();
+	const { t } = useTranslation();
+	const { siteConfig } = useSiteConfig();
+
+	const achievements = siteConfig.about?.achievements?.length > 0
+		? siteConfig.about.achievements
+		: FALLBACK_ACHIEVEMENTS;
+
 	return (
 		<section className="ls ms s-pt-lg-100 s-pb-lg-75 c-my-0 video-part right-part-bg text-center text-md-left" id="about">
 			<div className="cover-image s-cover-left"></div>
@@ -28,45 +41,17 @@ const HomeAbout = () => {
 							<img src={`${import.meta.env.BASE_URL}images/icon-3.png`} alt="" />
 						</div>
 						<div className="d-none d-lg-block divider-50"></div>
-						<div className="media">
-							<div className="icon-styled color-main2">
-								<i className="fa fa-trophy"></i>
+						{achievements.map((item, idx) => (
+							<div className="media" key={idx}>
+								<div className="icon-styled color-main2">
+									<i className={`fa ${item.icon || 'fa-star'}`}></i>
+								</div>
+								<div className="media-body">
+									<h5>{item.title}</h5>
+									<p>{item.desc}</p>
+								</div>
 							</div>
-							<div className="media-body">
-								<h5>
-									Đạt Hơn 50 Giải Thưởng Nấu Ăn
-								</h5>
-								<p>
-									Chứng nhận chất lượng xuất sắc, mang về các giải thưởng lớn nhỏ tại các cuộc thi làm bánh và nấu ăn trong nước cũng như quốc tế.
-								</p>
-							</div>
-						</div>
-						<div className="media">
-							<div className="icon-styled color-main2">
-								<i className="fa fa-group"></i>
-							</div>
-							<div className="media-body">
-								<h5>
-									27 Đầu Bếp - Chuyên Gia Đào Tạo
-								</h5>
-								<p>
-									Đội ngũ giảng viên giàu kinh nghiệm, tận tâm hướng dẫn và đồng hành cùng học viên từ những bước cơ bản nhất.
-								</p>
-							</div>
-						</div>
-						<div className="media">
-							<div className="icon-styled color-main2">
-								<i className="fa fa-hourglass-half"></i>
-							</div>
-							<div className="media-body">
-								<h5>
-									Cam Kết Việc Làm Nhanh Chóng
-								</h5>
-								<p>
-									Hỗ trợ định hướng nghề nghiệp, giới thiệu học viên đến các môi trường làm việc chuyên nghiệp ngay sau khi tốt nghiệp.
-								</p>
-							</div>
-						</div>
+						))}
 					</div>
 				</div>
 			</div>
