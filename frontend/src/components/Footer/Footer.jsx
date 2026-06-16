b006c01ea365251b199da91d92fe9f9ef6b789c3
Hoang Kien <hkien2310@gmail.com>
2026-06-16 11:10:58 +0700

    feat: add BCT notification logo to footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -106,6 +106,11 @@ const Footer = () => {
                   <SocialIconLink platform="tiktok" href={siteConfig.socials?.tiktok} title="tiktok" />
                   <SocialIconLink platform="youtube" href={siteConfig.socials?.youtube} title="youtube" />
                 </div>
+                <div className="mt-4">
+                  <a href="http://online.gov.vn/Home/WebDetails/143788" target="_blank" rel="noopener noreferrer">
+                    <img src="http://online.gov.vn/Content/EndUser/LogoCCDVSaleNoti/logoSaleNoti.png" alt="Đã thông báo Bộ Công Thương" style={{ maxWidth: '160px' }} />
+                  </a>
+                </div>
               </div>
             </div>
 
